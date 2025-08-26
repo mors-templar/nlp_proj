e11ebf4fe0f734d29db1fa12599508c8658dc399
mors-templar <raybaalb@gmail.com>
2025-08-26 17:12:36 +0500

    created the function to find the user text sentiment

--- sentiment_modal.py
+++ sentiment_modal.py
@@ -1,7 +1,21 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Aug 26 16:18:53 2025
+import textblob as tb
+import vaderSentiment as vader
 
-@author: rayb
-"""
-
+def get_user_sentiment(text):
+    tb_score = TextBlob(text).sentiment.polarity
+    vader_analyser = vader.vaderSentiment.SentimentIntensityAnalyzer()
+    v_scores = vader_analyser.polarity_scores(text)
+    v_cscores = v_scores["compound"]
+    
+    avg_score = (v_cscores + tb_scores) / 2
+    
+    if (avg_score > 0.15):
+        sentiment = "positive"
+    elif (avg_score < -0.15):
+        sentiment = "negative"
+    else:
+        sentiment = "neutral"
+    
+    return avg_score , sentiment
+        
+    
